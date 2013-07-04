cfaf088126da14e9f643ee9a07c4b2a8713a1eb6
Michael Gebetsroither <michael@mgeb.org>
2013-07-05 00:04:57 +0200

    streaming upload

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -11,31 +10,43 @@ import (
 	"path/filepath"
 )
 
+// Streams upload directly from file -> mime/multipart -> pipe -> http-request
+func asyncUploadFile(params map[string]string, paramName, path string, w *io.PipeWriter, file *os.File) {
+	defer file.Close()
+	defer w.Close()
+	writer := multipart.NewWriter(w)
+	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	_, err = io.Copy(part, file)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+
+	err = writer.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
 // Creates a new file upload http request with optional extra params
 func newfileUploadRequest(uri string, params map[string]string, paramName, path string) (*http.Request, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
 
-	body := &bytes.Buffer{}
-	writer := multipart.NewWriter(body)
-	part, err := writer.CreateFormFile(paramName, filepath.Base(path))
-	if err != nil {
-		return nil, err
-	}
-	_, err = io.Copy(part, file)
-
-	for key, val := range params {
-		_ = writer.WriteField(key, val)
-	}
-	err = writer.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	return http.NewRequest("POST", uri, body)
+	r, w := io.Pipe()
+	go asyncUploadFile(params, paramName, path, w, file)
+	return http.NewRequest("POST", uri, r)
 }
 
 func main() {
